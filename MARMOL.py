8e2dc1455f0a4386e8fe0ed9580873eca1a94efc
John Martin Marmol <marmoljohnmartin@gmail.com>
2026-02-28 21:37:23 +0800

    Add files via upload

---
+++ MARMOL.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 28 21:13:37 2026
+
+@author: marmo
+"""
+
+import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#
+mu = np.linspace(1.65, 1.8, num = 50)
+test = np.linspace(0, 2)
+uniform_dist = sts.uniform.pdf(mu) + 1
+
+uniform_dist = uniform_dist/uniform_dist.sum()
+beta_dist = sts.beta.pdf(mu, 2, 5, loc = 1.65, scale = 0.2)
+beta_dist = beta_dist/beta_dist.sum()
+plt.plot(mu, beta_dist, label = 'Beta Dist')
+plt.plot(mu, uniform_dist, label = 'Uniform Dist')
+plt.xlabel("Value of $\mu$ in meters")
+plt.ylabel("Probability density")
+plt.legend()
+
+
+#
+def likelihood_func(datum, mu):
+    likelihood_out = sts.norm.pdf(datum, mu, scale = 0.1)
+    return likelihood_out/likelihood_out.sum()
+
+likelihood_out = likelihood_func(1.7, mu)
+
+plt.plot(mu, likelihood_out)
+plt.title("Likelihood of $\mu$ given observation 1.7m")
+plt.ylabel("Probability Density/Likelihood")
+plt.xlabel("Value of $\mu$")
+plt.show()
+
+
+#
+import scipy as sp
+
+unnormalized_posterior = likelihood_out * uniform_dist
+plt.plot(mu, unnormalized_posterior)
+plt.xlabel("$\mu$ in meters")
+plt.ylabel("Unnormalized Posterior")
+plt.show()
+
+
+
+
+
